af16cd8428c033e977404a862b7a9ab6266f33ad
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-10-10 09:51:00 +0200

    extract "build_queued_msg()" helper function into generic "testutils" module

---
+++ schwarz/mailqueue/testutils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from io import BytesIO
+
+
+__all__ = ['build_queued_msg']
+
+def build_queued_msg(return_path=None, envelope_to=None, msg_bytes=None):
+    if return_path is None:
+        return_path = b'foo@site.example'
+    if envelope_to is None:
+        envelope_to = b'bar@site.example'
+    if msg_bytes is None:
+        msg_bytes = b'Header: somevalue\n\nMsgBody\n'
+    msg = (
+        b'Return-path: ' + return_path + b'\n'
+        b'Envelope-to: ' + envelope_to + b'\n' + \
+        msg_bytes
+    )
+    return BytesIO(msg)
--- tests/message_parsing_test.py
+++ tests/message_parsing_test.py
@@ -3,16 +3,15 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from io import BytesIO
-
 from pythonic_testcase import *
 
 from schwarz.mailqueue import parse_message_envelope
+from schwarz.mailqueue.testutils import build_queued_msg
 
 
 class MessageParsingTest(PythonicTestCase):
     def test_can_parse_simple_message_envelope(self):
-        queue_fp = self._msg_fp(
+        queue_fp = build_queued_msg(
             return_path=b'foo@site.example',
             envelope_to=b'bar@site.example',
             msg_bytes=b'RFC-821 MESSAGE',
@@ -24,33 +23,17 @@ class MessageParsingTest(PythonicTestCase):
 
     def test_can_parse_return_return_path_with_angle_brackets(self):
         # Exim puts angle brackets around the return path
-        queue_fp = self._msg_fp(return_path=b'<foo@site.example>')
+        queue_fp = build_queued_msg(return_path=b'<foo@site.example>')
         (from_addr, to_addrs, msg_fp) = parse_message_envelope(queue_fp)
         assert_equals('foo@site.example', from_addr)
 
     def test_can_parse_encoded_header(self):
         # repoze.sendmail encodes all (envelope) header values
-        queue_fp = self._msg_fp(
+        queue_fp = build_queued_msg(
             return_path=b'=?utf-8?q?foo=40site=2Eexample?=',
             envelope_to=b'=?utf-8?q?foo=2Ebar=40site=2Eexample?=',
         )
         (from_addr, to_addrs, msg_fp) = parse_message_envelope(queue_fp)
         assert_equals('foo@site.example', from_addr)
         assert_equals(('foo.bar@site.example',), to_addrs)
-
-
-    # --- internal helpers ----------------------------------------------------
-    def _msg_fp(self, return_path=None, envelope_to=None, msg_bytes=None):
-        if return_path is None:
-            return_path = b'foo@site.example'
-        if envelope_to is None:
-            envelope_to = b'bar@site.example'
-        if msg_bytes is None:
-            msg_bytes = b'RFC-821 MESSAGE'
-        msg = (
-            b'Return-path: ' + return_path + b'\n'
-            b'Envelope-to: ' + envelope_to + b'\n' + \
-            msg_bytes
-        )
-        return BytesIO(msg)
 
