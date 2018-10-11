225fb713b432d283c9a0dcc5473520116c84353c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-10-11 12:09:21 +0200

    SMTPMailerFullstackTest: rename variable
    It contains the message as it was *received* by the SMTP server. Usually
    this should be the same as the *sent* message but due to bugs this
    might not be the case.

--- tests/smtp_mailer_fullstack_test.py
+++ tests/smtp_mailer_fullstack_test.py
@@ -21,14 +21,14 @@ class SMTPMailerFullstackTest(SMTPTestCase):
         assert_true(msg_was_sent)
         received_queue = self.get_received_messages()
         assert_equals(1, received_queue.qsize())
-        sent_message = received_queue.get(block=False)
-        assert_equals(fromaddr, sent_message.smtp_from)
-        assert_equals(toaddrs, tuple(sent_message.smtp_to))
-        assert_none(sent_message.username)
+        received_message = received_queue.get(block=False)
+        assert_equals(fromaddr, received_message.smtp_from)
+        assert_equals(toaddrs, tuple(received_message.smtp_to))
+        assert_none(received_message.username)
         # pymta converts this to a string automatically
         expected_message = message.decode('ASCII')
         # in Python 2 the received message lacks the final '\n' (an unknown reason)
         if not IS_PYTHON3:
             expected_message = expected_message.rstrip('\n')
-        assert_equals(expected_message, sent_message.msg_data)
+        assert_equals(expected_message, received_message.msg_data)
 
