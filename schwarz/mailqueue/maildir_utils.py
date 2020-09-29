6836e326ff0536c4ef9dc77c273523705180e64f
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-09-29 12:20:45 +0200

    extract "MessageHandler._move_message()" into standalone utility function

--- schwarz/mailqueue/maildir_utils.py
+++ schwarz/mailqueue/maildir_utils.py
@@ -5,12 +5,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 
-from boltons.fileutils import atomic_save
+from boltons.fileutils import atomic_rename, atomic_save
 
 from .compat import os_makedirs
 
 
-__all__ = ['create_maildir_directories']
+__all__ = ['create_maildir_directories', 'move_message']
 
 def create_maildir_directories(basedir, is_folder=False):
     os_makedirs(basedir, 0o700, exist_ok=True)
@@ -32,3 +32,34 @@ def create_maildir_directories(basedir, is_folder=False):
         with atomic_save(maildirfolder_path, overwrite=False) as fp:
             pass
     return new_path
+
+
+def move_message(file_path, target_folder):
+    folder_path = os.path.dirname(file_path)
+    queue_base_dir = os.path.dirname(folder_path)
+    filename = os.path.basename(file_path)
+    target_path = os.path.join(queue_base_dir, target_folder, filename)
+    try:
+        # Bolton's "atomic_rename()" is compatible with Windows.
+        # Under Linux "atomic_rename()" ensures that the "target_path" file
+        # contains the complete contents AND never overwrites an existing
+        # file (as long as it is not stored on an NFS filesystem).
+        # However the full operation is NOT atomic in Linux as it consists of
+        # two system calls (link(), unlink()) so it could happen that the file
+        # exists in the source folder AND the target folder (as hard link).
+        # The ideal solution would be to use "renameat2", a Linux-specific
+        # system call which can rename without overwriting. However that
+        # syscall comes with a number of caveats:
+        # - not all file systems are supported (though I guess ext4 should be
+        #   fine)
+        # - not exposed in Python: need to write custom code
+        # - only added in glibc 2.28 (released on 2018-08-01) so we would
+        #   have to do a raw syscall from Python (doable, e.g. with the
+        #   "execute-syscall" github project)
+        # - added in Linux 3.15 - we can not use that syscall in CentOS 7
+        #   (ships with kernel 3.10) which is pretty much a showstopper for me.
+        atomic_rename(file_path, target_path, overwrite=False)
+        fp = open(target_path, 'rb+')
+    except (IOError, OSError):
+        fp = None
+    return fp
--- schwarz/mailqueue/message_handler.py
+++ schwarz/mailqueue/message_handler.py
@@ -6,8 +6,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import logging
 import os
 
-from boltons.fileutils import atomic_rename
-
+from .maildir_utils import move_message
 from .message_utils import parse_message_envelope
 
 
@@ -40,41 +39,10 @@ class MessageHandler(object):
         self.delivery_log.info(log_msg)
 
     def _mark_message_as_in_progress(self, source_path):
-        return self._move_message(source_path, target_folder='cur')
+        return move_message(source_path, target_folder='cur')
 
     def _move_message_back_to_new(self, source_path):
-        return self._move_message(source_path, target_folder='new')
-
-    def _move_message(self, file_path, target_folder):
-        folder_path = os.path.dirname(file_path)
-        queue_base_dir = os.path.dirname(folder_path)
-        filename = os.path.basename(file_path)
-        target_path = os.path.join(queue_base_dir, target_folder, filename)
-        try:
-            # Bolton's "atomic_rename()" is compatible with Windows
-            # Under Linux "atomic_rename()" ensures that the "target_path" file
-            # contains the complete contents AND never overwrites an existing
-            # file (as long as it is not stored on an NFS filesystem).
-            # However the full operation is NOT atomic in Linux as it consists
-            # of two system calls (link(), unlink()) so it could happen that
-            # the file exists in the source folder AND the target folder (as
-            # hard link).
-            # The ideal solution would be to use "renameat2", a Linux-specific
-            # system call which can rename without overwriting. However that
-            # syscall comes with a number of caveats:
-            # - not all file systems are supported (though I guess ext4 should
-            #   be fine)
-            # - not exposed in Python: need to write custom code
-            # - only added in glibc 2.28 (released on 2018-08-01) so we would
-            #   have to do a raw syscall from Python (doable, e.g. with the
-            #   "execute-syscall" github project)
-            # - added in Linux 3.15 - we can not use that syscall in CentOS 7
-            #   (ships with kernel 3.10) which is pretty much a showstopper for me.
-            atomic_rename(file_path, target_path, overwrite=False)
-            fp = open(target_path, 'rb+')
-        except (IOError, OSError):
-            fp = None
-        return fp
+        return move_message(source_path, target_folder='new')
 
     def _remove_message(self, fp):
         file_path = fp.name
