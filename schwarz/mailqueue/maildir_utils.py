FelixSchwarz/mailqueue-runner

commit 63d9ad52ee02575ab44ce255dccbb4b86314df95

drop support for Python 2

schwarz/mailqueue/maildir_utils.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MIT
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 import os
 
 import portalocker
 from boltons.fileutils import atomic_rename, atomic_save
 
-from .compat import IS_WINDOWS, FileNotFoundError, os_makedirs
+from .compat import IS_WINDOWS
 
 
 __all__ = ['create_maildir_directories', 'lock_file', 'move_message']
@@ -47,11 +45,11 @@ class LockedFile(object):
 
 
 def create_maildir_directories(basedir, is_folder=False):
-    os_makedirs(basedir, 0o700, exist_ok=True)
+    os.makedirs(basedir, 0o700, exist_ok=True)
     new_path = None
     for subdir_name in ('tmp', 'cur', 'new'):
         subdir_path = os.path.join(basedir, subdir_name)
-        os_makedirs(subdir_path, 0o700, exist_ok=True)
+        os.makedirs(subdir_path, 0o700, exist_ok=True)
         if subdir_name == 'new':
             new_path = subdir_path
 
@@ -68,15 +66,8 @@ def create_maildir_directories(basedir, is_folder=False):
     return new_path
 
 
-def is_path_like(path):
-    if hasattr(os, 'PathLike'):
-        return isinstance(path, os.PathLike)
-    # support Python 2 with pathlib2
-    return hasattr(path, '__fspath__')
-
-
 def find_messages(queue_basedir, log, queue_folder='new'):
-    if is_path_like(queue_basedir) and not hasattr(os, 'PathLike'):
+    if isinstance(queue_basedir, os.PathLike) and not hasattr(os, 'PathLike'):
         queue_basedir = str(queue_basedir)
     path_new = os.path.join(queue_basedir, queue_folder)
     try:

.github/workflows/tests.yml
@@ -11,26 +11,6 @@ on:
 
 
 jobs:
-  # --- Python 2 tests in separate container ----------------------------------
-  tests-py2:
-    runs-on: ubuntu-latest
-    container:
-      image: python:2.7.18-buster
-    timeout-minutes: 10
-
-    steps:
-      - uses: actions/checkout@v4
-
-      - name: Install dependencies
-        run: |
-          pip install --upgrade pip setuptools wheel
-          pip install -e .[testing]
-
-      - name: Run test suite
-        run: |
-          pytest
-
-
   # --- tests running on Ubuntu 20.04 -----------------------------------------
   tests-py36:
     # "ubuntu-latest" does not have Python 3.6

pytest.ini
@@ -1,5 +1,5 @@
 [pytest]
-#addopts = "--tb=native"
+addopts = "--tb=short"
 norecursedirs = .git build dist
 
 python_files =
@@ -20,7 +20,7 @@ filterwarnings =
     # puzzle plugins
     ignore:pkg_resources is deprecated as an API.:DeprecationWarning
 
-    # need to switch to Python 3-only namespace packages
+    # "schwarzlog" still uses pkg_resources for its namespace package
     ignore:Deprecated call to `pkg_resources.declare_namespace:DeprecationWarning
 
     # will need to update our usage here

schwarz/__init__.py
@@ -1,1 +1,0 @@
-__import__('pkg_resources').declare_namespace(__name__)

schwarz/mailqueue/app_helpers.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MIT
 
-from __future__ import absolute_import, print_function, unicode_literals
-
+import configparser
 import logging
 import logging.config
 import os
@@ -10,7 +9,6 @@ import sys
 
 from schwarz.puzzle_plugins import PluginLoader, parse_list_str
 
-from .compat import SafeConfigParser, configparser
 from .mailer import SMTPMailer
 from .plugins import registry
 
@@ -80,7 +78,7 @@ def parse_config(config_path, section_name=None):
         sys.stderr.write('config file "%s" not found.\n' % filename)
         sys.exit(20)
 
-    parser = SafeConfigParser()
+    parser = configparser.ConfigParser()
     exc_msg = None
     try:
         parser.read(config_path)

schwarz/mailqueue/cli.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MIT
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 import sys
 
 import docopt

schwarz/mailqueue/compat.py
@@ -1,101 +1,11 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MIT
 
-from __future__ import absolute_import, print_function, unicode_literals
-
-import email.utils
 import os
-import sys
 
 
 __all__ = [
-    'configparser',
-    'format_datetime_rfc2822',
-    'make_msgid',
-    'os_makedirs',
-    'queue',
-    'FileNotFoundError',
-    'SafeConfigParser',
-    'IS_PYTHON3',
     'IS_WINDOWS',
 ]
 
-IS_PYTHON3 = (sys.version_info >= (3,0))
 IS_WINDOWS = (os.name == 'nt')
-
-def os_makedirs(name, mode, exist_ok=False):
-    if IS_PYTHON3:
-        os.makedirs(name, mode, exist_ok=exist_ok)
-        return
-    # Python 2 version with "exist_ok=True" is racy but that is ok with my
-    # as Python 2 won't last that long.
-    if exist_ok is False:
-        os.makedirs(name, mode)
-    elif not os.path.exists(name):
-        os.makedirs(name, mode)
-
-try:
-    import configparser
-
-    # in Python 3.2 "SafeConfigParser" was renamed to "ConfigParser"
-    from configparser import ConfigParser as SafeConfigParser
-except ImportError:
-    import ConfigParser as configparser
-    from ConfigParser import SafeConfigParser
-
-try:
-    import queue
-except ImportError:
-    import Queue as queue
-
-try:
-    FileNotFoundError
-except NameError:
-    FileNotFoundError = OSError
-else:
-    FileNotFoundError = FileNotFoundError
-
-
-def format_datetime_rfc2822(dt):
-    try:
-        date_str = email.utils.format_datetime(dt)
-    except AttributeError:
-        # Python 2
-        # email.utils.formatdate() in Python always uses UTC timezone ("-0000")
-        # so the resulting string points to the same time but possibly in a
-        # different timezone.
-        # The naive version might something like
-        #   now_time = time.mktime(dt.timetuple())
-        #   date_str = email.utils.formatdate(now_time)
-        date_str = format_datetime_py2(dt)
-    return date_str
-
-
-WEEKDAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-MONTHS = (None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-
-def format_datetime_py2(dt):
-    tt = dt.timetuple()
-    utc_offset_str = dt.strftime('%z')
-    pattern = '%s, %02d %s %d %02d:%02d:%02d %s'
-    return  pattern % (
-        WEEKDAYS[tt.tm_wday],
-        tt.tm_mday,
-        MONTHS[tt.tm_mon],
-        tt.tm_year,
-        tt.tm_hour,
-        tt.tm_min,
-        tt.tm_sec,
-        utc_offset_str
-    )
-
-
-def make_msgid(domain=None):
-    if not domain:
-        return email.utils.make_msgid()
-    if sys.version_info >= (3, 2):
-        return email.utils.make_msgid(domain=domain)
-
-    msg_id_str = email.utils.make_msgid('@'+domain)
-    msg_id = msg_id_str.rsplit('@', 1)[0] + '>'
-    return msg_id

schwarz/mailqueue/mailer.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MIT
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 import socket
 from io import BytesIO
 from smtplib import SMTPException
@@ -18,8 +16,6 @@ class SMTPMailer(object):
         if (hostname is None) and ('client' not in kwargs):
             raise TypeError('not enough parameters for __init__(): please specify at least "hostname" or "client"')  # noqa: E501 (line too long)
         self.hostname = hostname
-        # ensure "port" is numeric as "socket.connect()" in Python 2 only
-        # accepts ints (in Python 3 '25' works as well).
         self.port = int(kwargs.pop('port', 25))
         self.username = kwargs.pop('username', None)
         self.password = kwargs.pop('password', None)

commit 3486a2ac343b4652b2d81cd41c2e2d5b2dd670db

remove condition which is not necessary since we only support Python 3
Fixes: 63d9ad52 ("drop support for Python 2")

schwarz/mailqueue/maildir_utils.py
@@ -67,8 +67,6 @@ def create_maildir_directories(basedir, is_folder=False):
 
 
 def find_messages(queue_basedir, log, queue_folder='new'):
-    if isinstance(queue_basedir, os.PathLike) and not hasattr(os, 'PathLike'):
-        queue_basedir = str(queue_basedir)
     if not os.path.exists(queue_basedir):
         log.error(f'Queue directory "{queue_basedir}" does not exist.')
     else: