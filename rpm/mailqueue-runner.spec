FelixSchwarz/mailqueue-runner

commit 68d683682fe341a279595663c2dc316797a78c36

rpm: add logrotate configuration

rpm/mailqueue-runner.spec
@@ -7,6 +7,9 @@ License:        MIT
 URL:            https://github.com/FelixSchwarz/mailqueue-runner
 Source:         %pypi_source
 Source1:        mailqueue-runner.conf
+Source2:        mailqueue-runner.logrotate
+
+Recommends:     logrotate
 
 BuildArch:      noarch
 BuildRequires:  python3-devel
@@ -53,6 +56,12 @@ rm -rf *.egg-info
     --parents \
     --mode=0700 \
     %{buildroot}%{_localstatedir}/log/mailqueue-runner
+/usr/bin/mkdir \
+    --parents \
+    --mode=0644 \
+    %{SOURCE2} \
+    %{buildroot}/%{_sysconfdir}/logrotate.d/mailqueue-runner.conf
+
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=1935266
 # namespace packages not fully supported ("schwarz/mailqueue" does not work)
@@ -77,6 +86,8 @@ restorecon %{_sysconfdir}/mailqueue-runner.conf
 %files -f %{pyproject_files}
 %doc README.md
 %config(noreplace) %{_sysconfdir}/mailqueue-runner.conf
+%dir %{_sysconfdir}/logrotate.d
+%config(noreplace) %{_sysconfdir}/logrotate.d/mailqueue-runner.conf
 %{_bindir}/mq-mail
 %{_bindir}/mq-run
 %{_bindir}/mq-send-test

rpm/mailqueue-runner.logrotate
@@ -1,0 +1,6 @@
+/var/log/mailqueue-runner/*log {
+    missingok
+    notifempty
+    compress
+    delaycompress
+}

commit 4854ee0494cb4b1cbd5504de4f4fc2168834e3a0

rpm: fix bad command (`install` instead of `mkdir`)
Fixes: 68d6836 ("rpm: add logrotate configuration")

rpm/mailqueue-runner.spec
@@ -56,9 +56,9 @@ rm -rf *.egg-info
     --parents \
     --mode=0700 \
     %{buildroot}%{_localstatedir}/log/mailqueue-runner
-/usr/bin/mkdir \
-    --parents \
+/usr/bin/install --preserve-timestamps \
     --mode=0644 \
+    -D \
     %{SOURCE2} \
     %{buildroot}/%{_sysconfdir}/logrotate.d/mailqueue-runner.conf
 